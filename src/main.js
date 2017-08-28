4b5c680638becae1c775bacf1296ce2e287953bf
cwchao <cwchao@soushi88.com>
2017-08-28 15:34:44 +0800

    react路由

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 /**
  * Created by songyingchun on 2017/8/25 0025.
  */
+import ReactDOM from "react-dom";
+import router from "@/router/router";
+
 import "@/config/flexible";
-import "@/router/router";
 import "@/style/common";
 import "@/style/test";
+
+ReactDOM.render(
+    router, document.getElementById("app")
+);
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,34 +1,52 @@
 /**
  * Created by songyingchun on 2017/8/25.
  */
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import {
-    BrowserRouter as Router,
+import React from "react";
+import Router,{
+    BrowserRouter,
+    HashRouter,
     Route,
     Link
 } from "react-router-dom";
 
-const Home = React.createClass({
-    render() {
-        return (<h3>
-            <Link to="/about">About</Link>
-            <Link to="/about">About</Link>
-        </h3>);
-    }
-});
+const env = process.env.NODE_ENV;
 
-const About = React.createClass({
-    render() {
-        return (<h3>About</h3>);
-    }
-});
+if(env === "development") {
 
-ReactDOM.render((
-    <Router>
+}
+
+const Home = () => (
+    <div>
+        <h2>Home123132</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
+
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+    </div>
+);
+
+const router = () => (
+    <HashRouter>
         <div>
-            <Route path="/" component={Home} />
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+            </ul>
+
+            <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
         </div>
-    </Router>
-), document.getElementById("app"));
+    </HashRouter>
+);
+
+export default router();
